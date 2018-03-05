00297849c986517f58f728797be6da1d9351c0e5
Yerson1995 <36700275+Yerson1995@users.noreply.github.com>
2018-03-05 08:27:33 -0500

    Add files via upload
    El objetivo de los codigos sobre Fibonnaci es evidenciar en que punto los distintos tipos de enteros de algunos lenguajes de programación presentan Overflow:
    El código en Java usa BigInteger para almacenar el valor de la N-esima posición de la secuencia de números de Fibonacci, ademas de esto muestra los números que van siendo generados, y para que posición de la secuencia existiría Overflow con los distintos tipos de entero

---
+++ JavaFibonnaci.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafibonnaci;
+
+import java.io.*;
+import java.math.BigInteger;
+public class JavaFibonnaci {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*System.out.println("Ingrese N");
+        String Ns = br.readLine();
+        int N = Integer.parseInt(Ns);
+        System.out.println(N);*/
+        byte nbytes=1;
+        BigInteger f0 = BigInteger.valueOf(0);
+        BigInteger f1 = BigInteger.valueOf(1);
+        for(int i=0;nbytes!=5;i++)
+        {
+
+            try
+            {
+                BigInteger aux = f1;
+                f1=f1.add(f0);
+                f0=aux;
+                switch(nbytes){
+                    case 1:
+                        if(f1.compareTo(BigInteger.valueOf(Byte.MAX_VALUE) )>0)
+                        {
+                           System.out.println("Overflow con Byte en "+(i-1));
+                           nbytes=2;
+                        }
+                        break;
+                    case 2:
+                        if(f1.compareTo(BigInteger.valueOf(Short.MAX_VALUE) )>0)
+                        {
+                           System.out.println("Overflow con Short en "+(i-1));
+                           nbytes=3;
+                        }
+                        break;
+                    case 3:
+                        if(f1.compareTo(BigInteger.valueOf(Integer.MAX_VALUE) )>0)
+                        {
+                           System.out.println("Overflow con Integer en "+(i-1));
+                           nbytes=4;
+                        }
+                        break;
+                    case 4:
+                        if(f1.compareTo(BigInteger.valueOf(Long.MAX_VALUE) )>0)
+                        {
+                           System.out.println("Overflow con Long en "+(i-1));
+                           nbytes=5;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                System.out.println(f1);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+
+        }
+    }  
+}
